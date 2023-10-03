62dfd4868c971af0ab93cc850b787c3e80fd4fda
Marko <marko@milicevic.fr>
2023-10-03 15:40:36 +0200

    Step 2: Integrate Login and Logout

--- FrontEnd/assets/scripts/page.js
+++ FrontEnd/assets/scripts/page.js
@@ -1,5 +1,8 @@
 // Global config
 const config = {
+	// LocalStorage key containing Credentials
+	// The versioning is a good practice because this data is stored at client-side
+	CREDENTIALS_LOCAL_STORAGE_KEY: "credentials-v1",
 	// API base URL (string: `protocol://domain:port`)
 	API_BASE_URL: "http://localhost:5678",
 	// Hard-coded default filtering Category's Id
@@ -8,6 +11,8 @@ const config = {
 
 // Global state
 const state = {
+	// Credentials of the logged user or null if the user is not logged
+	credentials: null,
 	// List of all available Works (Array<Work>)
 	works: [],
 	// List of all Categories from all available Works (Array<Category>)
@@ -15,3 +20,60 @@ const state = {
 	// Current category's Id to use for filtering (number or -1 for no filtering)
 	filteringByCategoryId: config.DEFAULT_ALL_CATEGORY_ID,
 };
+
+/**
+ * Show a page loader that is displayed over the entire page
+ * This page loader is recommended to be used during an HTTP query
+ */
+function showPageLoader() {
+	const bodyElement = document.querySelector("body");
+
+	const pageLoaderElement = document.createElement("div");
+	pageLoaderElement.className = "page-loader";
+	pageLoaderElement.innerHTML = `<i class="fa-solid fa-circle-notch fa-spin"></i>`;
+
+	bodyElement.appendChild(pageLoaderElement);
+}
+
+/**
+ * Hide a page loader
+ * This page loader is recommended to be used during an HTTP query
+ */
+function hidePageLoader() {
+	const pageLoaderElement = document.querySelector(".page-loader");
+	if (pageLoaderElement !== null) {
+		pageLoaderElement.remove();
+	}
+}
+
+/**
+ * Show a success Growl during few seconds
+ *
+ * @param {string} message Message to show
+ */
+function showSuccessGrowl(message) {
+	const bodyElement = document.querySelector("body");
+
+	const growlElement = document.createElement("div");
+	growlElement.className = "growl growl-success";
+	growlElement.innerText = message;
+	bodyElement.appendChild(growlElement);
+
+	window.setTimeout(() => growlElement.remove(), 10000);
+}
+
+/**
+ * Show an error Growl during few seconds
+ *
+ * @param {string} message Message to show
+ */
+function showErrorGrowl(message) {
+	const bodyElement = document.querySelector("body");
+
+	const growlElement = document.createElement("div");
+	growlElement.className = "growl growl-error";
+	growlElement.innerText = message;
+	bodyElement.appendChild(growlElement);
+
+	window.setTimeout(() => growlElement.remove(), 10000);
+}
---
+++ FrontEnd/assets/scripts/pages/login.js
@@ -0,0 +1,126 @@
+// Custom Errors for the login result
+class WrongCredentialsError extends Error {}
+class InternalServerError extends Error {}
+
+/**
+ * Call the API to get Credentials using email and password
+ *
+ * @returns {Promise<Credentials>} Successfully signed data
+ * @throws {WrongCredentialsError | InternalServerError | Error} Wrong crendentials | Internal error | Network error
+ */
+async function getCredentialsFromAPI(email, password) {
+	if (!email || !password) {
+		throw new Error("Invalid arguments");
+	}
+
+	return await fetch(`${config.API_BASE_URL}/api/users/login`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			email,
+			password,
+		}),
+	}).then((response) => {
+		switch (response.status) {
+			case 404:
+				throw new WrongCredentialsError();
+			case 200:
+				return response.json();
+			default:
+				throw new InternalServerError(`${response.status}`);
+		}
+	});
+}
+
+/**
+ * Remove the previous (if present) signin error message
+ */
+function removePreviousLoginErrorMessage() {
+	const errorMessageElement = document.querySelector("#login form .error-message");
+	if (errorMessageElement === null) {
+		return; // No previous error message
+	}
+	errorMessageElement.remove();
+}
+
+/**
+ * Show the signin error message
+ *
+ * @argument {String} errorMessage Error message to show
+ */
+function showLoginErrorMessage(errorMessage) {
+	const formElement = document.querySelector("#login form");
+
+	const errorMessageElement = document.createElement("p");
+	errorMessageElement.classList.add("error-message");
+	errorMessageElement.innerText = errorMessage;
+
+	formElement.prepend(errorMessageElement);
+}
+
+/**
+ * Store the crendentials inside the LocalStorage
+
+ * @param {Credentials} credentials Signed user's Credentials
+ */
+function setCredentialsIntoLocalStorage(credentials) {
+	window.localStorage.setItem(config.CREDENTIALS_LOCAL_STORAGE_KEY, JSON.stringify(credentials));
+}
+
+/**
+ * Bind the login form
+ */
+function bindLoginForm() {
+	const formElement = document.querySelector("#login form");
+
+	formElement.addEventListener("submit", async (event) => {
+		event.preventDefault(); // Prevent the default <form> submition behaviour
+
+		try {
+			showPageLoader();
+
+			removePreviousLoginErrorMessage();
+
+			const emailElement = formElement.querySelector('[name="email"]');
+			const passwordElement = formElement.querySelector('[name="password"]');
+
+			const credentials = await getCredentialsFromAPI(emailElement.value, passwordElement.value);
+
+			setCredentialsIntoLocalStorage(credentials);
+
+			redirectToHome();
+		} catch (err) {
+			if (err instanceof WrongCredentialsError) {
+				showLoginErrorMessage("Erreur dans l’identifiant ou le mot de passe");
+			} else if (err instanceof InternalServerError) {
+				showLoginErrorMessage("Une erreur est survenue, merci de réessayer dans un instant");
+			} else {
+				showLoginErrorMessage("Une erreur est survenue, merci de vérifier votre connectivité puis de réessayer");
+			}
+		} finally {
+			hidePageLoader();
+		}
+	});
+}
+
+/**
+ * Redirect the current user to the Home
+ */
+function redirectToHome() {
+	window.location.href = "../";
+}
+
+try {
+	if (window.localStorage.getItem(config.CREDENTIALS_LOCAL_STORAGE_KEY) !== null) {
+		// The user is already logged
+		redirectToHome();
+	} else {
+		// The user is not yet logged
+		bindLoginForm();
+	}
+} catch (err) {
+	showErrorGrowl("Navré, une erreur est survenue, merci de recharger la page dans quelques instants"); // TODO: Use i18n here
+	console.log(`An error occurred in login: ${err.message}`);
+}
